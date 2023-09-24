da1669ffee6a909bc66d82dd4458af8f60867a95
gcwarren <grace.warren@furman.edu>
2023-09-24 13:05:32 -0400

    return to original

--- src/global/Constants.java
+++ src/global/Constants.java
@@ -4,7 +4,7 @@ package global;
 public class Constants
 {
     public static final boolean DEBUG = true;
-   //test comment 
+     
     public static final String INPUT_FILE_COMMENT_PREFIX = "//";
 
     public static final String INPUT_FILE_EXTENSION = "json"; 
