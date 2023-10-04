6aec358337f2e2995c43d3588f31fbb435e85a48
Khushi Patel <khushipatel411@gmail.com>
2023-10-04 10:14:31 -0400

    All comments for JSONParser and tests

--- src/input/parser/JSONParser.java
+++ src/input/parser/JSONParser.java
@@ -39,46 +39,60 @@ public class JSONParser
 		
 		System.out.println(JSONroot);
 
+		//case for when JSON has no figure to parse
 		if (!JSONroot.has("Figure")) {
 			error("No figure found.");
 		}
+		//finds valid figure
 		JSONObject figure = JSONroot.getJSONObject("Figure");
 		
+		//reads description and points
 		String description = figure.getString("Description");
 		JSONArray pndb = figure.getJSONArray("Points");
 		JSONArray sndb = figure.getJSONArray("Segments");
 		
+		//reads into respective databases to create existing objects 
 		PointNodeDatabase pointNodeDatabase = readsPNDB(pndb);
 		SegmentNodeDatabase segmentNodeDatabase = readsSNDB(sndb, pointNodeDatabase);
 		
+		//creates a figure object with description and stored figure info
 		_astRoot = new FigureNode(description, pointNodeDatabase, segmentNodeDatabase);
 		return _astRoot;
 	}
 	
-	// TODO: implement supporting functionality
+	//method for JSON to read points from PointNode DB
 	public PointNodeDatabase readsPNDB(JSONArray pndbArray) {
+		//stores points in an array list
 		ArrayList<JSONObject> newPNDB = new ArrayList<JSONObject>();
 		PointNodeDatabase pointNodeDB = new PointNodeDatabase();
+		//loop to add points into a new DB
 		for (Object obj : pndbArray) {
 			newPNDB.add((JSONObject) obj);
 		}
+		//loop to return a point with a name and location 
 		for (JSONObject individual_node : newPNDB) {
 			String name = individual_node.getString("name");
 			Double x = individual_node.getDouble("x");
 			Double y = individual_node.getDouble("y");
+			//adds point to the DB
 			pointNodeDB.put(new PointNode(name, x, y));
 		}
 		return pointNodeDB;
 	}
 	
+	//method for JSON to read points from SegmentNode DB
 	public SegmentNodeDatabase readsSNDB(JSONArray sndbArray, PointNodeDatabase pointNodeDatabase) {
+		//stores segments in an array list 
 		ArrayList<JSONObject> newSNDB = new ArrayList<JSONObject>();
-		ArrayList<PointNode> adjacencyList = new ArrayList<PointNode>();
+		//ArrayList<PointNode> adjacencyList = new ArrayList<PointNode>();
+		
+		//create a new segment node DB to hold created segments
 		SegmentNodeDatabase segmentNodeDB = new SegmentNodeDatabase();
 		//put objects in array list
 		for (Object obj : sndbArray) {
 			newSNDB.add((JSONObject) obj);
 		}
+		//loop through the new segment DB to create segments from points in the point DB
 		for (JSONObject arrayObj : newSNDB) {
 			String key = arrayObj.keys().next();
 			JSONArray values = arrayObj.getJSONArray(key);
